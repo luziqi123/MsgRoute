65bd10d85b67fafb0519a3362405332879287bf1
runningmaggot@163.com <runningmaggot@163.com>
2017-04-01 22:18:22 +0800

    家里

--- app/src/main/java/com/simple/msg/TestActivity.java
+++ app/src/main/java/com/simple/msg/TestActivity.java
@@ -3,10 +3,6 @@ package com.simple.msg;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.longface.lazyui.DynamicView.DynamicView;
-import com.longface.lazyui.DynamicView.TypeView;
-
-import java.util.ArrayList;
 
 /**
  * Created by L on 2017/2/23.
@@ -18,11 +14,10 @@ public class TestActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-
-        DynamicView listView = (DynamicView)findViewById(R.id.listview);
-        listView.setData(new ArrayList<TypeView>());
-        listView.setItemClickable(true);
-        listView.setItemAnimator();
-        listView.bindType(R.layout.activity_main , new TypeTodo());
+//        DynamicView listView = (DynamicView)findViewById(R.id.listview);
+//        listView.setData(new ArrayList<TypeView>());
+//        listView.setItemClickable(true);
+//        listView.setItemAnimator();
+//        listView.bindType(R.layout.activity_main , new TypeTodo());
     }
 }
